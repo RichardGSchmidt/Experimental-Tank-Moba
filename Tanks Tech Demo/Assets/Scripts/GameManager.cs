513e15fcb8bb400c2071aa05b4ca8d1268e2c6e2
Richard G Schmidt Jr <richard.g.schmidt.jr@gmail.com>
2016-12-29 23:46:04 -0600

    Uploaded the proper package, removed an unrelated project.

---
+++ Tanks Tech Demo/Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Photon.MonoBehaviour {
+
+    public GameObject[] redSpawn;
+    public GameObject[] blueSpawn;
+
+    public int state = 0;
+
+    void Connect()
+    {
+        PhotonNetwork.ConnectToBestCloudServer("V-A1");
+        state = 1;
+    }
+
+
+    private void OnGUI()
+    {
+        switch (state)
+        {
+            case 0:
+                if (GUI.Button(new Rect(10, 10, 100, 30), "Connect"))
+                    Connect();
+                break;
+            case 1:
+                GUI.Label(new Rect(10, 10, 100, 30), "Connected");
+                break;
+        }
+    }
+}
